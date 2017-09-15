b836f321bc1064978865946e60ca3d46b69959a8
Shubhendra Vatsa <svatsa@DIN86000506.corp.capgemini.com>
2017-09-15 12:08:42 +0530

    Change in A.java

--- Project/A.java
+++ Project/A.java
@@ -1,5 +1,10 @@
 class A{
 public static void main(String args[]){
+
+
+Tester t = new Tester();
+t.getData();
+
 System.out.println("GIT ");
 System.out.println("DEVONE");
 System.out.println("DEVTWO");
